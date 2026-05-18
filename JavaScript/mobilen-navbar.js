e284199cad177392aa4083cb4ad9ebcba1efb6fd
kaykekkj <kaykekadaoka@gmail.com>
2026-05-18 09:12:56 -0300

    Delete JavaScript directory

--- JavaScript/mobilen-navbar.js
+++
@@ -1,7 +0,0 @@
-function AbrirMenu() {
-    if (document.getElementById("navlinks").style.transform == "translateX(0%)") {
-        document.getElementById("navlinks").style.transform = "translateX(100%)";
-    } else {
-        document.getElementById("navlinks").style.transform = "translateX(0%)";
-    }
-}
